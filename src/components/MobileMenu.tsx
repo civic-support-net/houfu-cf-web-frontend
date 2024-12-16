eb60baa0a0754e796cb329a4fbf1848deed3122d
icho-plus <slimkt623@gmail.com>
2024-12-16 17:25:01 +0900

    要素の中央揃えができていません。

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,10 +1,38 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { menus } from '@/consts/menus'
+import { cn } from '@/lib/utils'
+
 export default function MobileMenu() {
   return (
     <div
       className='absolute inset-8 overflow-auto rounded-2xl border-4 border-black bg-white shadow-lg'
       onClick={(e) => e.stopPropagation()}
     >
-      <nav className='p-4'>coding this area.</nav>
+      <nav className='p-4'>
+        <div className='mt-10 size-40 rounded-full bg-orange-300 p-6'>
+          <Link
+            href='/'
+            className='relative block size-full overflow-hidden rounded-full bg-primary'
+          >
+            <Image src='/img/logo.png' fill alt='logo' className='' />
+          </Link>
+        </div>
+        <div className='inset-0 flex'>
+          <ul className='items-center gap-4'>
+            {menus.map((menu, index) => (
+              <li key={index} className='my-3 flex items-start justify-center'>
+                <span className={cn('mr-2 mt-2 inline-block size-2 shrink-0', menu.bg_color)} />
+                <Link href={menu.href} className=''>
+                  {menu.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Image src='#' alt='image' width={300} height={300} />
+      </nav>
     </div>
   )
 }
--- src/consts/menus.ts
+++ src/consts/menus.ts
@@ -2,18 +2,21 @@ export const menus = [
   {
     label: 'おくる',
     bo_color: 'border-orange-400',
+    bg_color: 'bg-orange-400',
     tx_color: 'text-orange-500',
     href: '/provider',
   },
   {
     label: 'うけとる',
     bo_color: 'border-green-400',
+    bg_color: 'bg-green-400',
     tx_color: 'text-green-500',
     href: '/recipient',
   },
   {
     label: 'このサイトについて',
     bo_color: 'border-gray-700',
+    bg_color: 'bg-yellow-400',
     tx_color: 'text-gray-800',
     href: '/about',
   },
