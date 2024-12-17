7b3c37a9dd1a29f07665ca727b3da7b30029e547
icho-plus <slimkt623@gmail.com>
2024-12-17 21:16:20 +0900

    モバイルメニューの大まかな形を作成しました

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -10,8 +10,8 @@ export default function MobileMenu() {
       className='absolute inset-8 overflow-auto rounded-2xl border-4 border-black bg-white shadow-lg'
       onClick={(e) => e.stopPropagation()}
     >
-      <nav className='p-4'>
-        <div className='mt-10 size-40 rounded-full bg-orange-300 p-6'>
+      <nav className='flex flex-col items-center p-4'>
+        <div className='bg-orange-white mt-4 size-40 rounded-full p-4'>
           <Link
             href='/'
             className='relative block size-full overflow-hidden rounded-full bg-primary'
@@ -22,8 +22,21 @@ export default function MobileMenu() {
         <div className='inset-0 flex'>
           <ul className='items-center gap-4'>
             {menus.map((menu, index) => (
-              <li key={index} className='my-3 flex items-start justify-center'>
-                <span className={cn('mr-2 mt-2 inline-block size-2 shrink-0', menu.bg_color)} />
+              <li key={index} className='my-4 flex items-start justify-center'>
+                <div className='relative mr-4 mt-1 size-8 rotate-90'>
+                  <div
+                    className={cn(
+                      'absolute bottom-2 right-3 size-0 -scale-y-100 border-x-[12px] border-b-[24px] border-solid border-x-transparent',
+                      menu.bo_color,
+                    )}
+                  ></div>
+                  <div
+                    className={cn(
+                      'absolute bottom-0 left-1 size-0 -scale-y-100 border-x-4 border-t-8 border-x-transparent',
+                      menu.bo_color,
+                    )}
+                  ></div>
+                </div>
                 <Link href={menu.href} className=''>
                   {menu.label}
                 </Link>
@@ -31,7 +44,11 @@ export default function MobileMenu() {
             ))}
           </ul>
         </div>
-        <Image src='#' alt='image' width={300} height={300} />
+        <div className='mt-4 flex'>
+          <Image src='#' alt='image' width={100} height={100} />
+          <Image src='#' alt='image' width={100} height={100} />
+        </div>
+        <Image src='#' alt='image' width={100} height={100} />
       </nav>
     </div>
   )
