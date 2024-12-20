424afd66a8988fa39dd9bb6326df369ac7c6387d
icho-plus <slimkt623@gmail.com>
2024-12-20 18:13:19 +0900

    メガホンをimg配下に移動しました

--- src/consts/menus.ts
+++ src/consts/menus.ts
@@ -4,7 +4,7 @@ export const menus = [
     bo_color: 'border-orange-400',
     bg_color: 'bg-orange-400',
     tx_color: 'text-orange-500',
-    mg_href: '/hcf_megahon_okuru.png',
+    mg_href: '/img/hcf_megahon_okuru.png',
     href: '/provider',
   },
   {
@@ -12,7 +12,7 @@ export const menus = [
     bo_color: 'border-green-400',
     bg_color: 'bg-green-400',
     tx_color: 'text-green-500',
-    mg_href: '/hcf_megahon_uketoru.png',
+    mg_href: '/img/hcf_megahon_uketoru.png',
     href: '/recipient',
   },
   {
@@ -20,7 +20,7 @@ export const menus = [
     bo_color: 'border-gray-700',
     bg_color: 'bg-yellow-400',
     tx_color: 'text-gray-800',
-    mg_href: '/hcf_megahon.png',
+    mg_href: '/img/hcf_megahon.png',
     href: '/about',
   },
 ]
