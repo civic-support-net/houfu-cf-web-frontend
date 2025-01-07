6f939f875412514fc5d8c03b77fef288cc4142aa
hiroto <h_kado.2021@icloud.com>
2025-01-07 09:05:28 +0900

    1回目のレビューから変更したところ

--- src/components/EntryFlow.tsx
+++ src/components/EntryFlow.tsx
@@ -1,5 +1,5 @@
 interface RoundedRectangleProps {
-  circleText: string
+  circleText: number
 }
 
 const RoundedRectangle: React.FC<RoundedRectangleProps> = ({ circleText }) => {
@@ -51,25 +51,47 @@ const RoundedRectangle: React.FC<RoundedRectangleProps> = ({ circleText }) => {
 }
 
 const renderRoleText = ({ side }: { side: 'provider' | 'recipient' }) => {
-  const topText = side === 'provider' ? 'フードプレゼンター登録に登録してエールをおくる!' : 'うけとりメンバー登録をしてエールをおくる!'
-  const leftlabel = side === 'provider' ? '事前登録' : '電話で面談予約'
-  const centerlabel = side === 'provider' ? '物品のお持ち込み' : 'うけとりメンバー登録'
-  const rightlabel = side === 'provider' ? '確認のあと陳列' : '自由にうけとり'
-
-  const lefttext =
+  const topText =
     side === 'provider'
-      ? 'まずは、下記ボタンよりフードプレゼンター登録をお願いします。'
-      : 'まずは、電話でお問い合わせをしていただき、面談の時間を決めます。'
+      ? 'フードプレゼンター登録に登録してエールをおくる!'
+      : 'うけとりメンバー登録をしてエールをおくる!'
 
-  const centertext =
+  const flowContents =
     side === 'provider'
-      ? 'ルルサス防府2回「防府市市民活動登録センター受付」へ物品をお持ち込みください。'
-      : 'ご利用前に面談して、受け取りメンバー登録や、お手続き・ご利用方法の説明をします。'
-  const righttext =
-    side === 'provider'
-      ? '賞味期限などを確認した後抱負コミュニティブリッジへ陳列します。'
-      : 'お手持ちのスマートフォンに電子ロックの鍵を登録し、鍵を使って8:00〜21:30の間は'
-
+      ? [
+          {
+            label: '事前登録',
+            image: '',
+            text: 'まずは、下記ボタンよりフードプレゼンター登録をお願いします。',
+          },
+          {
+            label: '物品のお持ち込み',
+            image: '',
+            text: 'ルルサス防府2回「防府市市民活動登録センター受付」へ物品をお持ち込みください。',
+          },
+          {
+            label: '確認のあと陳列',
+            image: '',
+            text: '賞味期限などを確認した後抱負コミュニティブリッジへ陳列します。',
+          },
+        ]
+      : [
+          {
+            label: '電話で面談予約',
+            image: '',
+            text: 'まずは、電話でお問い合わせをしていただき、面談の時間を決めます。',
+          },
+          {
+            label: 'うけとりメンバー登録',
+            image: '',
+            text: 'ご利用前に面談して、受け取りメンバー登録や、お手続き・ご利用方法の説明をします。',
+          },
+          {
+            label: '自由にうけとり',
+            image: '',
+            text: 'お手持ちのスマートフォンに電子ロックの鍵を登録し、鍵を使って8:00〜21:30の間は',
+          },
+        ]
   const styleUnderline: React.CSSProperties = {
     textDecoration: 'underline',
     textDecorationColor: side === 'provider' ? '#E95513' : '#1EAA39',
@@ -77,19 +99,15 @@ const renderRoleText = ({ side }: { side: 'provider' | 'recipient' }) => {
   }
 
   return (
-    <div className='flex flex-col items-center justify-center gap-y-4 md:max-w-[768px]'>
+    <div className='mx-auto flex flex-col items-center justify-center gap-y-4 md:max-w-[768px]'>
       <div className='mx-auto flex text-center md:text-4xl'>
         <p className='text-center'>{topText}</p>
       </div>
 
       <div className='flex flex-col items-center justify-center gap-y-[28px] md:flex-row md:gap-x-[40px]'>
-        {[
-          { label: leftlabel, text: lefttext, circleText: '1' },
-          { label: centerlabel, text: centertext, circleText: '2' },
-          { label: rightlabel, text: righttext, circleText: '3' },
-        ].map(({ label, text, circleText }, index) => (
+        {flowContents.map(({ label, text }, index) => (
           <div key={index} className='flex max-w-[200px] flex-col items-center gap-y-4 break-words'>
-            <RoundedRectangle circleText={circleText} />
+            <RoundedRectangle circleText={index + 1} />
             <p className='items-center text-center text-base' style={styleUnderline}>
               {label}
             </p>
